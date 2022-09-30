d02cf1ddba9c443c3b2407e65e2ed1d625301a2a
Nicolás Rodríguez <18080513+nico-rodriguez@users.noreply.github.com>
2022-09-30 18:00:06 -0300

    React Native Basics: Core components

---
+++ src/components/Main.jsx
@@ -0,0 +1,20 @@
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: Constants.statusBarHeight,
+    flexGrow: 1,
+    flexShrink: 1,
+  },
+});
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Rate Repository Application</Text>
+    </View>
+  );
+};
+
+export default Main;
